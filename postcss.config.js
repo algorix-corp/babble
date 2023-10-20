7460c9eb5aa4d39b6c00d390a35298dcdc25ced8
YTG2G3 <scratchlover05@gmail.com>
2023-10-20 01:34:55 -0700

    git@github.com:AIGGEZ/babble.git

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+// tailwind.config.js
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    content: [
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: "class",
+    plugins: [nextui()],
+};
